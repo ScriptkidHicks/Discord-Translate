868cb3796b234959805e0f85ac3d7efac2d04586
Lillith Hicks <tammashicks@gmail.com>
2024-07-03 12:22:25 -0700

    Initial round of setup for the discord bot.

---
+++ DiscordTranslator/DiscordTranslator/Program.cs
@@ -0,0 +1,35 @@
+﻿using Client.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+// See https://aka.ms/new-console-template for more information
+
+namespace TranslatorBot {
+    internal class Program {
+        private static DiscordClient Client {get; set;}
+        private static CommandsNextExtension Commands {get; set;}
+        public static async Task Main(string[] args) {
+            var jsonReader = new JsonReader();
+            await jsonReader.ReadJson();
+
+            var discordConfig = new DiscordConfiguration {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args) {
+                return Task.CompletedTask;
+            }
+    }
+}
+
